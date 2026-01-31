3b723bf91794609a1d3e153c87cf1ec8c25f3e3e
nisashap99 <nisashap99@gmail.com>
2026-01-31 11:51:45 +0530

    expense-infra-jenkins

---
+++ 60-acm/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "domain_name" {
+    default = "nisarmeet.online"
+}
+
+variable "zone_id" {
+    default = "Z07534752MMQ5NVUBMPOB"
+}
